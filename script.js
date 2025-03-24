113588d0425cf984a4ac4ae8fb35c082d1b1a81e
PeterNgeno <peterkipsangngeno@gmail.com>
2025-03-25 01:48:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const PLAYLIST_ID = "PL358U6AJ8gNGj0v-M7n9W8PUJ-4fKv3SS"; // Your playlist ID
+const PLAYLIST_ID = "PL358U6AJ8gNHlktIp7Z2su-XrJT29LZtK"; // Replace with your actual ID
 const videoFrame = document.getElementById("videoFrame");
 
-// Set the YouTube playlist URL
 videoFrame.src = `https://www.youtube.com/embed/videoseries?list=${PLAYLIST_ID}&autoplay=1&loop=1&rel=0&modestbranding=1&playsinline=1`;
